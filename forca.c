54333cf263963123d8a336a2a5fbb44b477430b1
Diogo Sales Teixeira <72995630+Diogo0602x@users.noreply.github.com>
2021-03-19 16:43:10 -0300

    strlen criado
    Strlen criado para verificar se a letra falada pelo usuário existe.

--- forca.c
+++ forca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	char palavrasecreta[20];
@@ -9,7 +10,14 @@ int main() {
 	int enforcou = 1;
 
 	do {
-		// Começo do jogo!
-		
-	} while(!acertou && !enforcou);
-}
+    char chute;
+
+    printf("Qual letra? ");
+    scanf("%c", &chute);
+
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+        if(palavrasecreta[i] == chute) {
+            printf("A posição %d tem essa letra\n", i+1);
+        }
+    }
+} while (!acertou && !enforcou);
