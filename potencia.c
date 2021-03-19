529fe0e436b52939239995c64587c8f32a7ac045
Diogo Sales Teixeira <72995630+Diogo0602x@users.noreply.github.com>
2021-03-19 18:00:38 -0300

    void criado para retornar resultado

--- potencia.c
+++ potencia.c
@@ -1,9 +1,6 @@
-int potencia(int a, int b) {
-  int resultado = 1;
+void potencia(int* resultado, int a, int b) {
+  *resultado = 1;
   for(int i = 0; i < b; i++) {
-    resultado = resultado * a;
+    *resultado = *resultado * a;
   }
-
-  return resultado;
-
 }
